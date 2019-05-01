0339b24d7fa6cab5cb23373085f34c08a78eb53d
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2019-05-01 16:17:26 +0900

    Add example for Shared Memory

---
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/MP/examples/SharedMemory/Sub1/Sub1.ino
@@ -0,0 +1,44 @@
+/*
+ *  Sub1.ino - MP Example for MP Shared Memory
+ *  Copyright 2019 Sony Semiconductor Solutions Corporation
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <MP.h>
+
+#define MEMSIZE (128 * 1024) // 128KB
+
+void setup()
+{
+  int8_t msgid;
+  void *addr;
+
+  MP.begin();
+
+  /* Receive from MainCore */
+  MP.Recv(&msgid, &addr);
+
+  /* memory fill */
+  memset(addr, 0xaa, MEMSIZE);
+
+  /* Send address to MainCore */
+  MP.Send(msgid, addr);
+}
+
+void loop()
+{
+}
+
