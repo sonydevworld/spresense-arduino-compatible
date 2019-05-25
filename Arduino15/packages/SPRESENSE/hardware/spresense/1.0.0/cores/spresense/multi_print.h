ff32f4cd8dd2b3a9c96a77579e4385788fe91183
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2019-05-25 23:43:38 +0900

    Add printlog functions for MultiCore

---
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/cores/spresense/multi_print.h
@@ -0,0 +1,39 @@
+/*
+ *  multi_print.h - Spresense MultiCore printlog functions
+ *  Copyright 2019 Sony Semiconductor Solutions Corporation
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __MULTI_PRINT_H__
+#define __MULTI_PRINT_H__
+
+#include <sdk/config.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+void init_multi_print(void);
+void printlock(void);
+void printunlock(void);
+ssize_t uart_syncwrite(const char *buffer, size_t buflen);
+int sync_printf(const char *fmt, ...);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif  /* __MULTI_PRINT_H__ */
