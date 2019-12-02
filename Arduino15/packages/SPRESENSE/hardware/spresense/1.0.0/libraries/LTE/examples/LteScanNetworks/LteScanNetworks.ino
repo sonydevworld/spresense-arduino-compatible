b8d9933003deb736d9f81dd8527684d6529fefd7
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2019-12-02 11:49:35 +0900

    Add example for LTEScanner

---
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/LTE/examples/LteScanNetworks/LteScanNetworks.ino
@@ -0,0 +1,75 @@
+/*
+ *  LteScanNetworks.ino - Example for obtaining network information
+ *  Copyright 2019 Sony Semiconductor Solutions Corporation
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  This sketch turns on the modem and gets the following information:
+ *  - IP address assigned by LTE network
+ *  - Connected LTE network carrier
+ *  - Signal strength of the LTE network
+ *
+ */
+ 
+// libraries
+#include <LTE.h>
+
+// APN data
+#define LTE_APN       "apn"      // replace your APN
+#define LTE_USER_NAME "user"     // replace with your username
+#define LTE_PASSWORD  "password" // replace with your password
+
+// initialize the library instance
+LTE        lteAccess;
+LTEScanner scannerNetworks;
+
+void setup() {
+  // initialize serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  Serial.println("LTE networks scanner");
+
+  while (true) {
+    if (lteAccess.begin() == LTE_SEARCHING) {
+      if (lteAccess.attach(LTE_APN, LTE_USER_NAME, LTE_PASSWORD) == LTE_READY) {
+        Serial.println("attach succeeded.");
+        break;
+      }
+      Serial.println("An error occurred, shutdown and try again.");
+      lteAccess.shutdown();
+      sleep(1);
+    }
+  }
+}
+
+void loop() {
+   // Assigned IP address
+  IPAddress address = lteAccess.getIPAddress();
+  Serial.print("IP address: ");
+  Serial.println(address);
+
+  // currently connected carrier
+  Serial.print("Current carrier: ");
+  Serial.println(scannerNetworks.getCurrentCarrier());
+
+  // return signal strength
+  Serial.print("Signal Strength: ");
+  Serial.print(scannerNetworks.getSignalStrength());
+  Serial.println(" [dBm]");
+  sleep(1);
+}
