7d5fda0545723cc1a99298a883137ccc874848a9
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2020-07-28 13:16:45 +0900

    Fix spelling mistake

--- Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/SignalProcessing/src/FFT.h
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/SignalProcessing/src/FFT.h
@@ -32,8 +32,8 @@
 /*------------------------------------------------------------------*/
 /* WINDOW TYPE */
 typedef enum e_windowType {
-  WindoHamming,
-  WindoHanning,
+  WindowHamming,
+  WindowHanning,
   WindowRectangle
 } windowType_t;
 
@@ -44,7 +44,7 @@ template <int MAX_CHNUM, int FFTLEN> class FFTClass
 {
 public:
   void begin(){
-      begin(WindoHamming, MAX_CHNUM, (FFTLEN/2));
+      begin(WindowHamming, MAX_CHNUM, (FFTLEN/2));
   }
 
   bool begin(windowType_t type, int channel, int overlap){
@@ -119,9 +119,9 @@ private:
 
   void create_coef(windowType_t type){
     for (int i = 0; i < FFTLEN / 2; i++){
-      if(type == WindoHamming){
+      if(type == WindowHamming){
         coef[i] = 0.54f - (0.46f * arm_cos_f32(2 * PI * (float)i / (FFTLEN - 1)));
-      }else if(type == WindoHanning){
+      }else if(type == WindowHanning){
         coef[i] = 0.54f - (1.0f * arm_cos_f32(2 * PI * (float)i / (FFTLEN - 1)));
       }else{
         coef[i] = 1;
